a037a77f825ed650a1d336737905be330daa02b2
maunsolsson <Pontus@notvallens.se>
2025-09-25 09:06:36 +0200

    Added titles

--- R/plotreg.R
+++ R/plotreg.R
@@ -14,7 +14,7 @@ plot.reg <- function(x){
   resid <- x$e
 
   plot_df <- data.frame(e = resid, fits = x$fits, std_resid = resid / sqrt(x$residvar))
-  print(ggplot2::ggplot(plot_df) + ggplot2::geom_point(ggplot2::aes(x = fits, y = e)) + ggplot2::labs(main = "Resid vs fits", y = "Residuals", x = "Fitted values") + ggplot2::theme_bw())
-  print(ggplot2::ggplot(plot_df) + ggplot2::geom_point(ggplot2::aes(x = fits, y = sqrt(abs(std_resid)) )) + ggplot2::labs(main = "Scale-location", y = expression(sqrt("|Standardized residuals|")), x = "Fitted values") + ggplot2::theme_bw())
+  print(ggplot2::ggplot(plot_df) + ggplot2::geom_point(ggplot2::aes(x = fits, y = e)) + ggplot2::labs(title = "Resid vs fits", y = "Residuals", x = "Fitted values") + ggplot2::theme_bw())
+  print(ggplot2::ggplot(plot_df) + ggplot2::geom_point(ggplot2::aes(x = fits, y = sqrt(abs(std_resid)) )) + ggplot2::labs(title = "Scale-location", y = expression(sqrt("|Standardized residuals|")), x = "Fitted values") + ggplot2::theme_bw())
 }
 
